c45e6af065f7fb2547325edc02576fd7734df338
HyeJi Kim <137373@gmail.com>
2017-03-23 19:59:27 +0900

    modify

--- visualproject1/visualProject.py
+++ visualproject1/visualProject.py
@@ -3,7 +3,6 @@
 #Latest technology in visual computing Class
 #20143051 kim hye ji
 #Lab1.
-#python cjdmagotj zhemrk ejfjdnjdy
 ###############################################
 
 from __future__ import division
@@ -64,12 +63,14 @@ def DeMean(c, f):
 
 def Covariance(c, f1, f2):
     return Dot(DeMean(c,f1),DeMean(c,f2)) / (len(data[c][f1]) - 1)
-###################
-# return Mat
-##################
+
 def CovalMatrix(i,rowcol):    #covariance about class
     return [[round(Covariance(i, f1, f2), 2)
              for f2 in range(rowcol)] for f1 in range(rowcol)]
+
+def MeanMatrix():
+	return [[round(Mean(data[x][y]),2) 
+			for y in range(FEATURE)] for x in range(CLASSIFY)]
 
 def ReverseMatrix(C):
     return lin.inv(C)
@@ -77,15 +78,18 @@ def ReverseMatrix(C):
 def minusMatrix(a,b):
     return [a_i - b_i
             for a_i in a for b_i in b]
+
+
+
 ###################
-# Decision Boundary
+# Discriminant function
 ##################
-def DeciBoundary(testData, covar_, mean_):      # g(x) value (normal distribution) between testData and class
+def DiscriFunc(testData, covar_, mean_):      # g(x) value (normal distribution) between testData and class
     data = numpy.array(testData) #1XM
-    func = DeciBoundaryClass(covar_,mean_)
+    func = DiscriFuncClass(covar_,mean_)
     return Dot(Dot(data,func[2]),data.T) + Dot(func[1], data.T) + func[0]
 
-def DeciBoundaryClass(covar_, mean_):         # g(x) value with Class
+def DiscriFuncClass(covar_, mean_):         # g(x) value with Class
     CMat = numpy.array(covar_) #MXM
     RCMat = numpy.array(ReverseMatrix(CMat))
     mean = numpy.array(mean_) # 1XM
@@ -99,18 +103,18 @@ def DeciBoundaryClass(covar_, mean_):         # g(x) value with Class
     func.append((-1/2) * RCMat)
     return func
 
-def DeciBoundaryCompare(covar1, covar2, mean1, mean2):    #Decision Boundary between two Classes
+def DiscriFuncCompare(covar1, covar2, mean1, mean2):    #Discriminant function between two Classes
     func = []
-    func1 = DeciBoundaryClass(covar1,mean1)
-    func2 = DeciBoundaryClass(covar2,mean2)
+    func1 = DiscriFuncClass(covar1,mean1)
+    func2 = DiscriFuncClass(covar2,mean2)
 
     for i in range(CLASSIFY):
         func.append(func1[i] - func2[i])
     return func
 
-def DeciBoundaryTest(testData, covar1, covar2,mean1,mean2):    #Decision Boundary Test
+def DiscriFuncTest(testData, covar1, covar2,mean1,mean2):    #Discriminant function Test
     data = numpy.array(testData) #1X4
-    func = DeciBoundaryCompare(covar1,covar2, mean1,mean2)
+    func = DiscriFuncCompare(covar1,covar2, mean1,mean2)
     return Dot(Dot(data,func[2]),data.T) + Dot(func[1], data.T) + func[0]
 
 ###########################
@@ -121,9 +125,9 @@ def Classify(rowcol):
     for i in range(CLASSIFY):
         for j in range(len(test[i])):
             if(rowcol == 4):
-                clList = [DeciBoundary(test[i][j], CovalMatrix(k,4), meanData[k]) for k in range(CLASSIFY)]
+                clList = [DiscriFunc(test[i][j], CovalMatrix(k,4), meanData[k]) for k in range(CLASSIFY)]
             elif(rowcol == 2):
-                clList = [DeciBoundary(test[i][j][:2], CovalMatrix(k, 2), meanData[k][:2]) for k in range(CLASSIFY)]
+                clList = [DiscriFunc(test[i][j][:2], CovalMatrix(k, 2), meanData[k][:2]) for k in range(CLASSIFY)]
             result = (clList.index(max(clList)))
             cMat[i][result] += 1
 
@@ -133,14 +137,12 @@ def Classify(rowcol):
 fileOpen()
 testfileOpen()
 
-meanData = [[round(Mean(data[x][y]),2) for y in range(FEATURE)] for x in range(CLASSIFY)]
+meanData = MeanMatrix()
 
 #classified discriminant function
 print ('Problem1 confusion matrix')
 print(Classify(4))
-"""
-print(DeciBoundaryTest([5.0,3.6,1.4,0.2]))
-"""
+
 ########## Problem.2
 
 def mahalDist(testData, c):
@@ -156,6 +158,8 @@ def printDataPlot2X2():         #training data plot
 
     for i in range(CLASSIFY):
         plt.scatter(data[i][0], data[i][1], color = colorMat[i])
+        plt.scatter(meanData[i][0], meanData[i][1], marker='+', color = colorMat[i])
+
 
 def printTestPlot2X2():
     colorMat = ['red', 'black', 'green']
@@ -189,7 +193,7 @@ def DeciBoundaryPlot(c1, c2, m1, m2, color = 'black', rangeChange = 0):
     rangeY = arange(1.0, 5.0, 0.025)
 
     X, Y = meshgrid(rangeX, rangeY)
-    coefMat = (DeciBoundaryCompare(c1,c2,m1,m2))
+    coefMat = (DiscriFuncCompare(c1,c2,m1,m2))
 
     #quadratic coefficient(2X2 [a b c d])
     a = coefMat[2][0][0]
@@ -205,7 +209,7 @@ def DeciBoundaryPlot(c1, c2, m1, m2, color = 'black', rangeChange = 0):
     plt.contour(X, Y, testeq,[0], colors=color)
     return lambda X,Y: X * (a*X+c*Y+e) + Y * (b*X+d*Y+f)+g
 
-def compareFunc(func01, func12, func20):
+def testDataPlot(func01, func12, func20):
     colorMat = ['red', 'black', 'green']
 
     for c in range(3):
@@ -242,7 +246,7 @@ DBfunc12 = DeciBoundaryPlot(CoMat2X2[1],CoMat2X2[2], meanData[1][:2], meanData[2
 DBfunc20 = DeciBoundaryPlot(CoMat2X2[2],CoMat2X2[0], meanData[2][:2], meanData[0][:2],'black')
 
 #classified using decision boundary, plot
-compareFunc(DBfunc01,DBfunc12,DBfunc20)
+testDataPlot(DBfunc01,DBfunc12,DBfunc20)
 
 #classified discriminant function
 print ('Problem2 confusion matrix')
